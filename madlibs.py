b196946568080f07a807af761f1edff31cf78d1d
user <user@192.168.1.17>
2022-03-07 18:47:11 +0100

    adding first try of a simple  madlib

---
+++ madlibs.py
@@ -0,0 +1,12 @@
+#a simple self presentation madlib
+
+name = input("what is your name?  ")
+artist = input("who is your favorite artist?  ")
+music = input("what type of music you like?  ") 
+food = input("what is your favorite food?  ")
+food2 = input("what is your inliked food?  ")
+learn = input("what are you currently learning?  ")
+
+
+madlib = f"Hi! My name is {name} I would like to talk about my self a little.\nI like to listen to {artist} and the music genre i love is {music}\nMy favorite food would be {food} and i hate {food2} curently I am learning {learn}. I hope all mine and your dreams come true"
+print(madlib)
