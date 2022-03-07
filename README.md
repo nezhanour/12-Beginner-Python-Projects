2149fbe81871a9c4e4074ad65d5eaec6bf188284
user <user@192.168.1.17>
2022-03-07 19:04:59 +0100

    correcting some spelling

--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 for 12 days I'll be doing a 12 beginner projects and post them on github as part of my 100 days of code challenge   
 
 source of the projects : https://www.youtube.com/watch?v=8ext9G7xspg&amp;t=1s
+
+# first project: madlib
+I made a simple madlib as a small self presentation. Don't judge, I have absolutely not ideas  
--- madlibs.py
+++ madlibs.py
@@ -4,9 +4,9 @@ name = input("what is your name?  ")
 artist = input("who is your favorite artist?  ")
 music = input("what type of music you like?  ") 
 food = input("what is your favorite food?  ")
-food2 = input("what is your inliked food?  ")
+food2 = input("what is your least favorite food?  ")
 learn = input("what are you currently learning?  ")
 
 
-madlib = f"Hi! My name is {name} I would like to talk about my self a little.\nI like to listen to {artist} and the music genre i love is {music}\nMy favorite food would be {food} and i hate {food2} curently I am learning {learn}. I hope all mine and your dreams come true"
+madlib = f"Hi! My name is {name} I would like to talk about myself a little.\nI like to listen to {artist} and the music genre I love is {music}\nMy favorite food would be {food} and I hate {food2}, currently I am learning {learn}.\nI hope all mine and your dreams come true."
 print(madlib)
